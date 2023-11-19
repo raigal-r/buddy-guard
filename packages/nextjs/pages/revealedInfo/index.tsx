393dd5796235b09047f78c3c4b5f0c15facfa011
raigal-r <raquel.raigal@outlook.es>
2023-11-19 04:48:18 +0300

    removed commented code

--- packages/nextjs/pages/revealedInfo/index.tsx
+++ packages/nextjs/pages/revealedInfo/index.tsx
@@ -1,52 +1,9 @@
 import type { NextPage } from "next";
 
 const RevealedInfo: NextPage = () => {
-  // const { error } = useFetchBlocks();
-
-  // useEffect(() => {
-  //   if (getTargetNetwork().id === hardhat.id && error) {
-  //     notification.error(
-  //       <>
-  //         <p className="font-bold mt-0 mb-1">Cannot connect to local provider</p>
-  //         <p className="m-0">
-  //           - Did you forget to run <code className="italic bg-base-300 text-base font-bold">yarn chain</code> ?
-  //         </p>
-  //         <p className="mt-1 break-normal">
-  //           - Or you can change <code className="italic bg-base-300 text-base font-bold">targetNetwork</code> in{" "}
-  //           <code className="italic bg-base-300 text-base font-bold">scaffold.config.ts</code>
-  //         </p>
-  //       </>,
-  //     );
-  //   }
-
-  //   if (getTargetNetwork().id !== hardhat.id) {
-  //     notification.error(
-  //       <>
-  //         <p className="font-bold mt-0 mb-1">
-  //           <code className="italic bg-base-300 text-base font-bold"> targeNetwork </code> is not localhost
-  //         </p>
-  //         <p className="m-0">
-  //           - You are on <code className="italic bg-base-300 text-base font-bold">{getTargetNetwork().name}</code> .This
-  //           block explorer is only for <code className="italic bg-base-300 text-base font-bold">localhost</code>.
-  //         </p>
-  //         <p className="mt-1 break-normal">
-  //           - You can use{" "}
-  //           <a className="text-accent" href={getTargetNetwork().blockExplorers?.default.url}>
-  //             {getTargetNetwork().blockExplorers?.default.name}
-  //           </a>{" "}
-  //           instead
-  //         </p>
-  //       </>,
-  //     );
-  //   }
-  // }, [error]);
-
   return (
     <div className="container mx-auto mt-4 mb-4">
       <div className="w-full flex flex-col gap-3 mx-5">
-        {/* <div className="flex relative w-40 h-48 ml-0">
-          <Image alt="buddy-joy" fill src="/buddy-rc.png" />
-        </div> */}
         <div className="flex flex-col gap-7 ml-0 mr-5">
           <div className="text-6xl font-medium text-red-500 ">S.O.S</div>
           <div className="flex flex-col gap-8 text-[1.4rem] font-medium bg-red-500 text-white p-4 py-10 rounded-md">
