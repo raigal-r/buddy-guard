54391c22671fc0bcd1e623e57e9360f4d32ec51e
raigal-r <raquel.raigal@outlook.es>
2023-11-19 05:46:07 +0300

    push aborted

--- packages/nextjs/components/DirectMessage.tsx
+++ packages/nextjs/components/DirectMessage.tsx
@@ -1,20 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { type Env, MessageWithCID, PushAPI } from "@pushprotocol/restapi";
-import { ethers } from "ethers";
+import { Wallet, ethers } from "ethers";
+import { sepolia } from "wagmi";
 
 const DirectMessage: React.FC = () => {
-  //   const [message, setMessage] = useState<MessageWithCID | null>(null); // change state variable type
+  //   const privateKey = process.env.DEPLOYER_PRIVATE_KEY;
+  //   const network = sepolia;
 
+  //   const [message, setMessage] = useState<MessageWithCID | null>(null); // change state variable type
+  //   const provider = new ethers.providers.JsonRpcProvider(network.rpcUrls.alchemy.http[0]);
+  //     const signer = new ethers.Wallet(privateKey, provider);
   //   useEffect(() => {
   //     const initializeAndSendMessage = async () => {
-  //       const signer = ethers.Wallet.createRandom();
-
   //       // Initialize wallet user, pass 'prod' instead of 'staging' for mainnet apps
   //       const userAlice = await PushAPI.initialize(signer, { env: "staging" as Env });
 
   //       // Send a message to Bob
-  //       const aliceMessagesBob = await userAlice.chat.send("0x072d7e87c13bCe2751B5766A0E2280BAD235974f", {
+  //       const aliceMessagesBob = await userAlice.chat.send("0xaaD254C50ebA6f779b431d6B882fCEA135822f93", {
   //         content: "Gm gm! It's a me... Mario",
   //       });
 
@@ -24,13 +27,7 @@ const DirectMessage: React.FC = () => {
   //     initializeAndSendMessage();
   //   }, []);
 
-  return (
-    <>
-      <div>
-        <p>Hola</p>
-      </div>
-    </>
-  );
+  return <></>;
 };
 
 export default DirectMessage;
